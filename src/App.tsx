4570965365d07b0c30eb3b6c76bf0d79d0a79fb6
Kai <kaiqianghuang666@gmail.com>
2024-06-06 15:35:02 +0100

    connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  THEME,
+  TonConnectButton,
+  TonConnectUIProvider,
+} from "@tonconnect/ui-react";
 
 import WebApp from "@twa-dev/sdk";
 
@@ -10,6 +15,42 @@ function App() {
 
   return (
     <>
+      <TonConnectUIProvider
+        manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+        uiPreferences={{ theme: THEME.DARK }}
+        walletsListConfiguration={{
+          includeWallets: [
+            {
+              appName: "safepalwallet",
+              name: "SafePal",
+              imageUrl:
+                "https://s.pvcliping.com/web/public_image/SafePal_x288.png",
+              tondns: "",
+              aboutUrl: "https://www.safepal.com",
+              universalLink: "https://link.safepal.io/ton-connect",
+              jsBridgeKey: "safepalwallet",
+              bridgeUrl: "https://ton-bridge.safepal.com/tonbridge/v1/bridge",
+              platforms: ["ios", "android", "chrome", "firefox"],
+            },
+            {
+              appName: "tonwallet",
+              name: "TON Wallet",
+              imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+              aboutUrl:
+                "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+              universalLink: "https://wallet.ton.org/ton-connect",
+              jsBridgeKey: "tonwallet",
+              bridgeUrl: "https://bridge.tonapi.io/bridge",
+              platforms: ["chrome", "android"],
+            },
+          ],
+        }}
+        actionsConfiguration={{
+          twaReturnUrl: "https://t.me/DemoDappWithTonConnectBot/demo",
+        }}
+      >
+        <TonConnectButton />
+      </TonConnectUIProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
